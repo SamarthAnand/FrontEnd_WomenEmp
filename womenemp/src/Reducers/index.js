f6d6ebcfd79ba3fe2b0fea5c95203b972e0790ba
Kumar Singh <gaurav.kumar-singh@capgemini.com>
2022-11-09 13:36:24 +0530

    login and SignUP

--- womenemp/src/Reducers/index.js
+++ womenemp/src/Reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { selectedTraineeReducer, traineeReducer } from "./TraineeReducers";
+import { userReducer } from "./UserReducer";
 
 const allReducers = combineReducers({
     allTrainees : traineeReducer,
-    trainee : selectedTraineeReducer
+    trainee : selectedTraineeReducer,
+    user : userReducer
 })
 
 export default allReducers;
