865e9b4b1ec85b11dd0f3221137232d809b212f8
Kumar Singh <gaurav.kumar-singh@capgemini.com>
2022-11-09 23:50:59 +0530

    trainee enroll in course

--- womenemp/src/Components/Scheme/SchemesView.js
+++ womenemp/src/Components/Scheme/SchemesView.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { updateTrainee } from '../../Actions/TraineeActions';
 
 export default function SchemesView({ schemes }) {
+    const dispatch = useDispatch();
+    const trainee = useSelector((state) => state.trainee)
+    const handleClick = (course)=> {
+        trainee.trainingCourse = course;
+        dispatch(updateTrainee(trainee))
+        alert("successfully enrolled")
+    }
     return (
         <div className='tableContainer'>  
             <div className="grid" id="schemeHead">
@@ -10,6 +19,8 @@ export default function SchemesView({ schemes }) {
                 <div>Eligibility</div>
                 <div>Launch Date</div>
                 <div>Type</div>
+                <div>Course Name</div>
+                <div>Enroll</div>
 
             </div>
             {schemes && schemes.map((scheme) => (
@@ -20,7 +31,8 @@ export default function SchemesView({ schemes }) {
                     <div>{scheme.schemeEligibility}</div>
                     <div>{scheme.launchDate}</div>
                     <div>{scheme.schemeType}</div>
-
+                    <div>{scheme.trainingCourse.courseName}</div>
+                    <div><button className='buttonBlue' onClick={() =>handleClick(scheme.trainingCourse)}>Enroll</button></div>
                 </div>
             ))}
         </div>
