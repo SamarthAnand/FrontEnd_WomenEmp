bd5471f4bcd4a821383e4c09f086528c3605fdab
speedbar <nabeelthenutty@gmail.com>
2022-11-10 22:24:28 +0530

    feedback latest

--- womenemp/src/Components/Feedback/AddFeedback.js
+++ womenemp/src/Components/Feedback/AddFeedback.js
@@ -3,18 +3,22 @@ import "../../Style/style.css";
 import axios from 'axios';
 import {useNavigate} from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
+import { fetchSchemes } from "../../Actions/SchemeActions";
+import Select from 'react-select';
 
 function Feedback(){ 
+    
     const dispatch= useDispatch();
     const currUser = useSelector((state) => state.user);
     const d = new Date().toISOString().split("T")[0];
     const navigate = useNavigate();
     const [isSubmit, setIsSubmit] = useState(false);
-    const initialValues = {feedBackId:100,schemeRating:null,schemeTrainingRating:null,overallRating:null,user:{},comment:"",date:d};
+    const initialValues = {feedBackId:100,schemeRating:null,schemeTrainingRating:null,overallRating:null,user:{},schemeName:"",comment:"",date:d};
     const [formValues, setFormValues] = useState(initialValues);
     const [formErrors, setFormErrors] = useState({});
-   
+    const schemes = useSelector((state) => state.allSchemes.schemes);
 
+  
     const handleChange = (e)=>{
         const {name, value} = e.target;
         setFormValues({...formValues, [name] : value});
@@ -23,6 +27,9 @@ function Feedback(){
         e.preventDefault();
         setFormErrors(validate(formValues));
         setIsSubmit(true)
+    }
+    const getScheme =()=>{
+      dispatch(fetchSchemes())
     }
     useEffect(()=>{
         console.log(formErrors)
@@ -61,11 +68,11 @@ function Feedback(){
     return(
 
         <div className="feedback">
-            {/* {Object.keys(formErrors).length === 0 && isSubmit ? (
+            {Object.keys(formErrors).length === 0 && isSubmit ? (
           <div className="ui message success">Feedback added succesfully</div>
         ) : (
           <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
-        )} */}
+        )}
             <h2>Give your Feedback</h2>
             <form onSubmit={handleSubmit}>
                 {/* <div><label>FeedbackID</label>
@@ -80,7 +87,17 @@ function Feedback(){
                     <label>Scheme Rating</label>
                     <input type="number" name="overallRating"></input>
                     </div> */}
-                    <div>
+              <div>
+                <label>Scheme Name</label>
+                <select
+                required
+                name="schemeName"
+                onChange={handleChange} onClick={getScheme}>
+                    <option  disabled selected hidden>Select your option</option>
+                    {schemes && schemes.map((dat)=>(
+                      <option>{dat.schemeName}</option>
+                    ))}
+                </select>
                         <label>Scheme Rating</label>
                 <select
                 required
@@ -98,6 +115,7 @@ function Feedback(){
                     <option>9</option>
                     <option>10</option>
                 </select>
+                
                 
                     <label 
                     >Scheme Training Rating</label>
@@ -154,166 +172,3 @@ function Feedback(){
     )
 }
 export default Feedback;
-
-
-// import React, { useEffect, useState } from "react";
-// import "../../Style/style.css";
-// import axios from "axios";
-// import { useNavigate } from "react-router-dom";
-// import { useDispatch, useSelector } from "react-redux";
-// import { updateTrainee } from "../../Actions/TraineeActions";
-// import Nav from "../Nav"
-
-// function Feedback() {
-//   // const trainee = useSelector((state) => state.trainee)
-//   const currUser = useSelector((state) => state.user);
-//   // const dispatch = useDispatch()
-//   const d = new Date().toISOString().split("T")[0];
-//   const navigate = useNavigate();
-//   const [isSubmit, setIsSubmit] = useState(false);
-//   const initialValues = {
-//     feedBackId: 100,
-//     schemeRating: null,
-//     schemeTrainingRating: null,
-//     overallRating: null,
-//     comment: "",
-//     date: d,
-//     user: {},
-//   };
-//   const [formValues, setFormValues] = useState(initialValues);
-//   const [formErrors, setFormErrors] = useState({});
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormValues({ ...formValues, [name]: value });
-//   };
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     setFormErrors(validate(formValues));
-//     setIsSubmit(true);
-//   };
-//   useEffect(() => {
-//     // console.log(formErrors)
-//     if (Object.keys(formErrors).length === 0 && isSubmit) {
-//       formValues.user = currUser;
-//       axios.post(`http://localhost:8202/api/Feedback`, formValues).then(() => {
-//         alert("Feedback Submitted");
-//         navigate("/home");
-//       });
-//     }
-//   }, [formErrors]);
-
-//   const validate = (value) => {
-//     const errors = {};
-//     if (!value.schemeRating) {
-//       errors.schemeRating = "Please provide Scheme Rating";
-//     }
-//     if (!value.schemeTrainingRating) {
-//       errors.schemeTrainingRating = "Please provide Scheme Training Rating";
-//     }
-//     if (!value.overallRating) {
-//       errors.overallRating = "Please provide Overall Rating";
-//     }
-//     if (!value.comment) {
-//       errors.comment = "Please provide Comments";
-//     }
-//     return errors;
-//   };
-
-//   return (
-//     <div>
-//       <Nav />
-//       <div className="feedback">
-//         {/* {Object.keys(formErrors).length === 0 && isSubmit ? (
-//           <div className="ui message success">Feedback added succesfully</div>
-//         ) : (
-//           <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
-//         )} */}
-//         <h2>Give your Feedback</h2>
-//         <form onSubmit={handleSubmit}>
-//           {/* <div><label>FeedbackID</label>
-//                 <input type="number" 
-//                 name="feedBackId"
-//                 onChange={handleChange}></input></div> */}
-//           {/* <div>
-                    
-//                     <input type="number" name="schemeRating"></input>
-//                     <label>Scheme Rating</label>
-//                     <input type="number" name="schemeTrainingRating"></input>
-//                     <label>Scheme Rating</label>
-//                     <input type="number" name="overallRating"></input>
-//                     </div> */}
-//           <div>
-//             <label>Scheme Rating</label>
-//             <select required name="schemeRating" onChange={handleChange}>
-//               <option value="" disabled selected hidden>
-//                 Select your option
-//               </option>
-//               <option>1</option>
-//               <option>2</option>
-//               <option>3</option>
-//               <option>4</option>
-//               <option>5</option>
-//               <option>6</option>
-//               <option>7</option>
-//               <option>8</option>
-//               <option>9</option>
-//               <option>10</option>
-//             </select>
-
-//             <label>Scheme Training Rating</label>
-//             <select
-//               required
-//               name="schemeTrainingRating"
-//               onChange={handleChange}
-//             >
-//               <option value="" disabled selected hidden>
-//                 Select your option
-//               </option>
-//               <option>1</option>
-//               <option>2</option>
-//               <option>3</option>
-//               <option>4</option>
-//               <option>5</option>
-//               <option>6</option>
-//               <option>7</option>
-//               <option>8</option>
-//               <option>9</option>
-//               <option>10</option>
-//             </select>
-
-//             <label>Overall Rating</label>
-//             <select required name="overallRating" onChange={handleChange}>
-//               <option value="" disabled selected hidden>
-//                 Select your option
-//               </option>
-//               <option>1</option>
-//               <option>2</option>
-//               <option>3</option>
-//               <option>4</option>
-//               <option>5</option>
-//               <option>6</option>
-//               <option>7</option>
-//               <option>8</option>
-//               <option>9</option>
-//               <option>10</option>
-//             </select>
-
-//             <label>Comments</label>
-//             <textarea
-//               required
-//               type="text"
-//               onChange={handleChange}
-//               name="comment"
-//               placeholder="Add comments"
-//             ></textarea>
-
-//             <button role="button">Submit</button>
-//           </div>
-//         </form>
-//         <div className="feedbackAfter" id="feedbackAfter"></div>
-//       </div>
-//     </div>
-//   );
-// }
-// export default Feedback;
--- womenemp/src/Components/Feedback/FeedbackById.js
+++ womenemp/src/Components/Feedback/FeedbackById.js
@@ -1,48 +1,62 @@
-// import React, { useEffect } from 'react'
-// import { useDispatch, useSelector } from 'react-redux'
-// import { useNavigate } from 'react-router-dom'
-// import { fetchFeedback } from '../../Actions/FeedbackActions'
-// import { deleteTrainee, fetchTrainee } from '../../Actions/TraineeActions'
-// import { deleteUser } from '../../Actions/UserAction'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { fetchFeedback } from '../../Actions/FeedbackActions'
+import { deleteTrainee, fetchTrainee } from '../../Actions/TraineeActions'
+import { deleteUser } from '../../Actions/UserAction'
 
-// function TraineeById() {
+function FeedbackById({data}) {
 
-//     const feedback = useSelector((state) =>state.feedback);
-//     const dispatch = useDispatch();
-//     const navigate = useNavigate();
-//     useEffect(()=>{
-//         fetchFeedback(user.feedbackId)
-//     },[])
-
-//     const handleUpdate = ()=> {
-//         navigate("/updatefeedback")
-//     }
-//   return (
-//     <div>
-//         <table style={{margin: "10px auto"}}>
+  return (
+    
+    <div>
+        <table class="table table-striped">
+            <tbody>
+            <tr>
+                <td>Feedback ID :</td>
+                <td>{data.feedBackId}</td>
+            </tr>
+            <tr>
+                <td>Scheme Rating :</td>
+                <td>{data.schemeRating}</td>
+            </tr>
+            <tr>
+                <td>Training Rating :</td>
+                <td>{data.schemeTrainingRating}</td>
+            </tr>
+            <tr>
+                <td>Overall Rating :</td>
+                <td>{data.overallRating}</td>
+            </tr>
+            <tr>
+                <td>User :</td>
+                <td>
+                    {data.user ? data.user.userId: "null"}</td>
+            </tr>
+            <tr>
+                <td>Scheme :</td>
+                <td>
+                    {data.scheme ? data.scheme.schemeName: "null"}</td>
+            </tr>
+            <tr>
+                <td>Training Course :</td>
+                <td>
+                    {data.trainingCourse ? data.training.trainingCourse: "null"}</td>
+            </tr>
+            <tr>
+                <td>Comments :</td>
+                <td>{data.comment}</td>
+            </tr>
             
             
-            
-            
-//             <tr>
-//                 <td>DOB :</td>
-//                 <td>{feedback.comment}</td>
-//             </tr>
-//             <tr>
-//                 <td>Training Course :</td>
-//                 <td>{trainee.trainingCourse ? trainee.trainingCourse.courseName: "Null"}</td>
-//             </tr>
-//             <tr>
-//                 <td>Feedback : </td>
-//                 <td>{trainee.feedback ? trainee.feedback.overallRating: "Null"}</td>
-//             </tr>
-//         </table>
+            </tbody>
+        </table>
+{/* 
+        <button className='buttonBlue' onClick={handleUpdate}>Update</button>
+        <button className='buttonBlue' onClick={handleDelete} style={{backgroundColor:"Red"}}>Delete</button> */}
 
-//         <button className='buttonBlue' onClick={handleUpdate}>Update</button>
-//         <button className='buttonBlue' onClick={handleDelete} style={{backgroundColor:"Red"}}>Delete</button>
+    </div>
+  )
+}
 
-//     </div>
-//   )
-// }
-
-export default TraineeById
+export default FeedbackById
